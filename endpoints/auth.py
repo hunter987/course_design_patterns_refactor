d7c6bbfe93867b3e6fb763029e3a04d5bb81dd2f
Brayan Diaz Arrieta <bdiaz043161@gmail.com>
2025-11-30 18:27:59 -0500

    refactor(auth): delegate token generation to AuthService

--- endpoints/auth.py
+++ endpoints/auth.py
@@ -1,16 +1,17 @@
 from flask import Blueprint, request
 from flask_restful import Resource, Api
+from utils.auth_service import AuthService
 
 class AuthenticationResource(Resource):
     def post(self):
         username = request.json.get('username')
         password = request.json.get('password')
 
-        if username == 'student' and password == 'desingp':
-            token = 'abcd12345'
-            return {'token': token}, 200
-        else:
+        token = AuthService.generate_token(username, password)
+        if not token:
             return {'message': 'unauthorized'}, 401
+
+        return {'token': token}, 200
 
 
 
